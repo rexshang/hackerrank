7cc67d416e7988186dfc85e69cf214865caf6f91
rexshang <rexshang@10.152.120.229>
2016-12-14 11:34:48 -0800

    update project

--- red_john2.py
+++ red_john2.py
@@ -5,57 +5,75 @@ Created on Dec 8, 2016
 
 @author: erexsha
 '''
-memo = {}
 
-def find_coin(t, coin, soln, solns):
+import math
+
+memo = {}
+primes = {}
+
+last_prime = 0
+prime_cnt = 0
+
+def find_primes(upper):
+    global last_prime
+    global prime_cnt
     
-    ways = 0
-    for i in coin:
-        remain = t - i
-        soln.append(i)
-        if remain > 0:
-            #if remain in memo.keys():
-            #    ways += memo[remain]
-            #else:
-            #memo[remain] = find_coin(remain, coin, soln, solns)
-            ways += find_coin(remain, coin, soln, solns)
-        elif remain == 0:
-            s = ''.join(str(e) for e in soln)
-            solns[s] = 1
-            ways += 1
-#            print soln
+    #combo = 217286
+    for num in range(last_prime, upper + 1):
+        # prime_cnt numbers are greater than 1
+        if num > 1:
+            for i in range(2, int(math.sqrt(num)) + 1):
+                if (num % i) == 0:
+                    break
+            else:
+                prime_cnt += 1
+        primes[num] = prime_cnt
+        
+    last_prime = upper + 1
+    
+    
+def find_ways(n):
+    if n in memo:
+        return memo[n]
+
+    if n == 0:
+        return 0
+    elif n < 4:
+        return 1
+    else:
+        
+        if (n-4) in memo:
+            f4 = memo[n-4]
         else:
-            pass
-            #coin = coin[1:]
-            #soln.pop()
-            #break
-        soln.pop()
+            f4 = find_ways(n-4)
+            memo[n-4] = f4
             
-    
-    return ways
+        if (n-1) in memo:
+            f1 = memo[n-1]
+        else:
+            f1 = find_ways(n-1)
+            memo[n-1] = f1
+            
+        ways = f4 + f1
+        memo[n] = ways           
+        return ways
+
+memo[1] = 1
+memo[2] = 1
+memo[3] = 1
+memo[4] = 2
+
+find_primes(101)
 
 tc = int(raw_input().strip())
 
 for _ in range(tc):
     target= int(raw_input().strip())
     
-    coin = [4, 1]
+    ways = find_ways(target)
+    if ways > last_prime:
+        find_primes(ways)
+        
+    #print ways
+    print primes[ways]
     
-    soln = []
-    solns = {}
-    
-    find_coin(target, coin, soln, solns)
-    print "combo: " +  str(len(solns))
-    print solns
-    combo = len(solns)
-    
-    prime = 0
-    for num in range(0, combo + 1):
-        # prime numbers are greater than 1
-        if num > 1:
-            for i in range(2,num):
-                if (num % i) == 0:
-                    break
-            else:
-                prime += 1
-    print prime
